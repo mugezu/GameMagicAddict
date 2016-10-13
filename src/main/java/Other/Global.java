c4f883ddd66f5415340494748cb2071c2c3130eb
Mugezu <mugezu2013@gmail.com>
2016-10-13 20:15:58 +0300

    Save/Load Skill and Hero

---
+++ src/main/java/Other/Global.java
@@ -0,0 +1,10 @@
+package Other;
+
+import java.io.File;
+
+/**
+ * Created by user on 13.10.2016.
+ */
+public class Global {
+    public static String currentPath= new File("").getAbsolutePath();
+}
